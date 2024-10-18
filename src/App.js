6f39753deb3bb780d6352e9196ffd6ba3d876c75
Shamini1606 <shaminisiva16@gmail.com>
2024-10-18 23:31:23 +0530

    Updated react-http 18 october

--- src/App.js
+++ src/App.js
@@ -1,8 +1,128 @@
+// import { useEffect, useState } from "react";
+// import "./App.css";
+// import { json } from "react-router-dom";
+
+// import { Button, EditableText, InputGroup, Toaster } from "@blueprintjs/core";
+// import { web } from "webpack";
+
+// const AppToaster = Toaster.create({
+//   position: "top",
+// });
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+//   const [newName, setNewName] = useState("");
+//   const [newEmail, setNewEmail] = useState("");
+//   const [newWebsite, setNewWebsite] = useState("");
+
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((json) => setUsers(json));
+//   }, []);
+
+//   function addUser() {
+//     const name = newName.trim();
+//     const email = newEmail.trim();
+//     const website = newWebsite.trim();
+
+//     if (name && email && website) {
+//       fetch("https://jsonplaceholder.typicode.com/users", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           name,
+//           email,
+//           website,
+//         }),
+//         headers: {
+//           "Content-Type": "application/json charset=UTF-8 ",
+//         },
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           setUsers([...users, data]);
+//           AppToaster.show({
+//             message: "user added successfully",
+//             intent: "success",
+//             timeout: 3000,
+//           });
+//         });
+//     }
+//   }
+
+//   return (
+//     <div className="App">
+//       <table className="bp4-html-table modifier">
+//         <thead>
+//           <th>ID</th>
+//           <th>Name</th>
+//           <th>Email</th>
+//           <th>Website</th>
+//           <th>Action</th>
+//         </thead>
+//         <tbody>
+//           {users.map((user) => (
+//             <tr key={user.id}>
+//               <td>{user.id}</td>
+//               <td>{user.name}</td>
+//               <td>
+//                 <EditableText value={user.email} />
+//               </td>
+//               <td>
+//                 <EditableText value={user.website} />
+//               </td>
+//               <td>
+//                 <Button intent="primary">Update</Button>
+//                 <Button intent="danger">Delete</Button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//         <tfoot>
+//           <tr>
+//             <td></td>
+//             <td>
+//               <InputGroup
+//                 value={newName}
+//                 onChange={(e) => setNewName(e.target.value)}
+//                 placeholder="Enter Name..."
+//               />
+//             </td>
+//             <td>
+//               <InputGroup
+//                 value={newEmail}
+//                 onChange={(e) => setNewEmail(e.target.value)}
+//                 placeholder="Enter Email..."
+//               />
+//             </td>
+//             <td>
+//               <InputGroup
+//                 value={newWebsite}
+//                 onChange={(e) => setNewWebsite(e.target.value)}
+//                 placeholder="Enter Website..."
+//               />
+//             </td>
+//             <td>
+//               <Button intent="success" onClick={addUser}>
+//                 Add User
+//               </Button>
+//             </td>
+//           </tr>
+//         </tfoot>
+//       </table>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useEffect, useState } from "react";
 import "./App.css";
-import { json } from "react-router-dom";
+import { Button, EditableText, InputGroup, Toaster } from "@blueprintjs/core";
 
-import { Button, EditableText, InputGroup } from "@blueprintjs/core";
+const AppToaster = Toaster.create({
+  position: "top",
+});
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -16,15 +136,46 @@ function App() {
       .then((json) => setUsers(json));
   }, []);
 
+  function addUser() {
+    const name = newName.trim();
+    const email = newEmail.trim();
+    const website = newWebsite.trim();
+
+    if (name && email && website) {
+      fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          email,
+          website,
+        }),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUsers([...users, data]);
+          AppToaster.show({
+            message: "User added successfully",
+            intent: "success",
+            timeout: 3000, // Corrected type
+          });
+        });
+    }
+  }
+
   return (
     <div className="App">
       <table className="bp4-html-table modifier">
         <thead>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Website</th>
-          <th>Action</th>
+          <tr> {/* Added missing <tr> */}
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Website</th>
+            <th>Action</th>
+          </tr>
         </thead>
         <tbody>
           {users.map((user) => (
@@ -32,10 +183,10 @@ function App() {
               <td>{user.id}</td>
               <td>{user.name}</td>
               <td>
-                <EditableText value="{user.email}" />
+                <EditableText value={user.email} /> {/* Corrected interpolation */}
               </td>
               <td>
-                <EditableText value="{user.website}" />
+                <EditableText value={user.website} /> {/* Corrected interpolation */}
               </td>
               <td>
                 <Button intent="primary">Update</Button>
@@ -69,7 +220,9 @@ function App() {
               />
             </td>
             <td>
-              <Button intent="success">Add User</Button>
+              <Button intent="success" onClick={addUser}>
+                Add User
+              </Button>
             </td>
           </tr>
         </tfoot>
